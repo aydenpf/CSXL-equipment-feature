faf9e4c5dd13f195403e9cca3ac16a1d7755f76d
Jacob Brown <jsbrown@unc.edu>
2023-11-10 12:10:22 -0500

    Add dependency on registered user in equipment API

--- backend/api/equipment/checkout.py
+++ backend/api/equipment/checkout.py
@@ -5,8 +5,11 @@ This API is used to manage and list user equipment checkouts"""
 from fastapi import APIRouter, Depends, HTTPException
 
 from backend.models.equipment_type import EquipmentType
+from backend.models.user import User
 from ...models.equipment import Equipment
 from ...services.equipment import EquipmentService
+
+from backend.api.authentication import registered_user
 
 __authors__ = ["Nicholas Mountain", "Jacob Brown", "Ayden Franklin", "David Sprague"]
 __copyright__ = "Copyright 2023"
@@ -27,7 +30,9 @@ def get_all(equipment_service: EquipmentService = Depends()) -> list[Equipment]:
 
 @api.put("/update", tags=["Equipment"])
 def update(
-    item: Equipment, equipment_service: EquipmentService = Depends()
+    item: Equipment,
+    equipment_service: EquipmentService = Depends(),
+    subject: User = Depends(registered_user),
 ) -> Equipment:
     """Updates an item of equipment and returns the updated item as an equipment model"""
     return equipment_service.update(item)
@@ -36,6 +41,7 @@ def update(
 @api.get("/get_all_types", tags=["Equipment"])
 def get_all_types(
     equipment_service: EquipmentService = Depends(),
+    subject: User = Depends(registered_user),
 ) -> list[EquipmentType]:
     """Gets all unique equipment types from database with correct available inventory and returns to the user as a list of equipment type models"""
     return equipment_service.get_all_types()
